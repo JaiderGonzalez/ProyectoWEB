bfb6fcdbd8bb1380e897428dc5deb81cbeb00b7e
JaiderGonzalez <jaidergonzalez601@gmail.com>
2023-07-15 16:07:36 -0500

    responsive

--- src/components/jsx/NavBar.jsx
+++ src/components/jsx/NavBar.jsx
@@ -3,20 +3,22 @@ import "../css/NavBar.css"
 import { BsFillCloudSunFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <>
         <nav>
-            <div className="nav-logo-container">
-                <a href="">AIRSCAPE</a>
-            </div>
-            <div className="navbar-links-container">
+            <div className="nav-logo-container">AIRSCAPE</div>
+            <div className={`navbar-links-container ${isOpen && "open"}`}>
                 <a href="">Servicios</a>
                 <a href="">Informacion</a>
                 <a href="">Centro de Ayuda</a>
-                <a href="">
-                    <BsFillCloudSunFill className="navbar-cart-icon"/>
-                </a>
-                <Link className="primary-button" to="/register">Register</Link>
+                <a href=""><BsFillCloudSunFill className="navbar-cart-icon"/></a>
+                <Link className="primary-button" to="/register">Sign Up</Link>
+            </div>
+            <div className={`nav-toggle ${isOpen && "open"}`}
+            onClick={() => setIsOpen(!isOpen)}
+            >
+                <div className="bar"></div>
             </div>
         </nav>
         </>
